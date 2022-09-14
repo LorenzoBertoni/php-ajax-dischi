559c145b7c8e6b4d53b347371485c28613331b2f
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-14 16:10:32 +0200

    add: funzione di filtraggio in 'apiCall.php' + seconda chiamata axios con parametri get in base alla value della option. TODO: fix bug nella condizione di filtraggio: restituisce solo 1 oggetto, indipendentemente dal fatto che ci siano più oggetti nell'array.

--- client_side/apiCall.php
+++ client_side/apiCall.php
@@ -1,4 +1,17 @@
 <?php
     include __DIR__ . '/../database.php';
     header('Content-Type: application/json');
-    echo json_encode($database);
+    if(isset($_GET['genre'])) {
+        $genre = $_GET['genre'];
+        foreach($database as $disc) {
+            if ($disc['genre'] == $genre) {
+                $disc['isFiltered'] = true;
+            }
+            if ($disc['isFiltered']) {
+                $database = $disc;
+            }
+        }
+        echo json_encode($database);
+    } else {
+        echo json_encode($database);
+    }
--- client_side/main.js
+++ client_side/main.js
@@ -1,7 +1,19 @@
 const app = new Vue({
     el:'#app',
     data: {
-        discs: []
+        discs: [],
+        filteredDiscs: [],
+        genreValue: ''
+    },
+    methods: {
+        getFilter() {
+            this.filteredDiscs = []
+            axios.get('http://localhost/php-ajax-dischi/client_side/apiCall.php?genre=' + this.genreValue)
+            .then(filteredResponse => {
+                this.filteredDiscs.push(filteredResponse.data)
+                console.log(this.filteredDiscs)
+            })
+        }
     },
     created() {
         axios.get('http://localhost/php-ajax-dischi/client_side/apiCall.php')
